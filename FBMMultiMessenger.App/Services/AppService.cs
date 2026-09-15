77b460923ad7525fd35b0f88ce753a0517373a04
zubair khakwani <zbrkhakwani@gmail.com>
2026-09-15 10:14:16 +0500

    c

--- FBMMultiMessenger.App/Services/AppService.cs
+++ FBMMultiMessenger.App/Services/AppService.cs
@@ -21,7 +21,7 @@ namespace FBMMultiMessenger.Services
             {
                 var client = new HttpClient();
 
-                string jsonBase64 = await client.GetStringAsync("https://github.com/ShaheerKhawajikzai/FBM-MultiMessenger-Resources/releases/download/v1.0.0/version.json");
+                string jsonBase64 = await client.GetStringAsync("https://github.com/zubairkhakwani/FBM-MultiMessenger-Resources/releases/download/v1.0.0/version.json");
 
                 var remoteVersion = JsonSerializer.Deserialize<RemoteVersion>(jsonBase64, new JsonSerializerOptions
                 {
@@ -83,6 +83,7 @@ namespace FBMMultiMessenger.Services
             }
             catch (Exception ex)
             {
+                SentrySdk.CaptureException(ex);
             }
         }
 
